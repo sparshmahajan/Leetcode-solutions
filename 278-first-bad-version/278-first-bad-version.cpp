f5a28e4c4343402c853d58f481830808894d1eb2
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-12 23:41:41 +0530

    Time: 4 ms (12.72%), Space: 6 MB (21.65%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -7,10 +7,12 @@ public:
         return recursiveSearch(0,n);
     }
     int recursiveSearch(int start , int end){
-        if(isBadVersion(end-1) == false) return end;
+        if(isBadVersion(end-1) == false) 
+            return end;
         int mid = start + (end-start)/2;
         if(isBadVersion(mid)==true){
-            if(isBadVersion(mid-1)==false) return mid;
+            if(isBadVersion(mid-1)==false) 
+                return mid;
             else return recursiveSearch(start,mid-1);
         }
         else return recursiveSearch(mid+1,end);
