d9d9a5fb71b0d7f5fc5cc2f59184a36fea523598
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-12 22:48:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (70.26%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -7,7 +7,7 @@ public:
         while((n%2==0 or n%3==0 or n%5==0)){
             if(n%2 == 0) n/=2;
             if(n%3 == 0) n/=3;
-            if(n%5 == 0) n/=5;
+            if(n%5==0) n/=5;
         }
         return n==1;
     }
