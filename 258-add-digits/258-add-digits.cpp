cd006b4aa2463850ddd7234c0b6cdcb928ace7fd
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-12 22:34:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (23.16%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int addDigits(int num) {
         if(num < 9) return num;
-        if(num%9 == 0) return 9;
+        if(num%9==0) return 9;
         return num%9;
     }
 };
