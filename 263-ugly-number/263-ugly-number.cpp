15a6d456cd5d066f3c967f8177a4fffa9ca3f5e9
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-12 22:48:16 +0530

    Time: 4 ms (34.97%), Space: 5.9 MB (21.21%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<1) return false;
+        if(n==1) return true;
+        if(!(n%2==0 or n%3==0 or n%5==0)) return false;
+        while((n%2==0 or n%3==0 or n%5==0)){
+            if(n%2 == 0) n/=2;
+            if(n%3 == 0) n/=3;
+            if(n%5 == 0) n/=5;
+        }
+        return n == 1;
+    }
+};
