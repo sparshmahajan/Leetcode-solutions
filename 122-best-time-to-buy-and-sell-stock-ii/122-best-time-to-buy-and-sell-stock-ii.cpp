2b790060a312a91568b3de01a52ad81d96dbf780
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-15 23:32:00 +0530

    Time: 21 ms (5.56%), Space: 13 MB (46.53%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,8 +3,7 @@ public:
     int maxProfit(vector<int>& prices) {
         int maxProfit = 0;
         for(int i=0;i<prices.size()-1;i++){
-            if(prices[i]<prices[i+1])
-                maxProfit += prices[i+1] - prices[i];
+            if(prices[i]<prices[i+1]) maxProfit += prices[i+1] - prices[i];
         }
         return maxProfit;
     }
