36d39ad0022df0a47a278c7d55d28901accb0930
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-17 17:55:42 +0530

    Time: 96 ms (9.88%), Space: 19.6 MB (67.02%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i=0, count1=0,count2=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                i++;
+                count1++;
+            }
+        }
+        i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }
+            else{
+                j--;
+                count2++;
+            }
+        }
+        if(count1==1 or count2==1) return true;
+        if(count1==0 or count2==0) return true;
+        return false;
+    }
+};
