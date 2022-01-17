26a6691358803144f5c9aa0bcce61749eac65cb0
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-17 19:38:44 +0530

    Time: 93 ms (5.06%), Space: 28.4 MB (41.77%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -3,6 +3,6 @@ public:
     int minMoves(vector<int>& nums) {
         int total=accumulate(nums.begin(),nums.end(),0);
         int n=nums.size(),minimum=*min_element(nums.begin(),nums.end());
-        return total - (n*minimum);
+        return total-(n*minimum);
     }
 };
