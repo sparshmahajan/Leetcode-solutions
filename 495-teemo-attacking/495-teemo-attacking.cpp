7bc0befd64310a8c8148db148c834256cea5a561
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-19 01:29:21 +0530

    Time: 51 ms (41.41%), Space: 25.9 MB (52.32%) - LeetHub

---
+++ 495-teemo-attacking/495-teemo-attacking.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int sum = 0;
+        for(int i=1;i<timeSeries.size();i++){
+            if(timeSeries[i-1] + duration <= timeSeries[i])
+                sum += duration;
+            else
+                 sum += (timeSeries[i]-timeSeries[i-1]);
+        }
+        sum += duration;
+        return sum;
+    }
+};
