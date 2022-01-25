bf8533bb80b0e12c968f8b9f20823657065610d2
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-01-25 14:17:44 +0530

    Time: 41 ms (35.27%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        if(arr.size()<3) return 0;
+        if(arr.size()<3) 
+            return 0;
         int res=0,i;
         for(i=0;i<arr.size()-1;i++){
             if(arr[i]>=arr[i+1]) break;
