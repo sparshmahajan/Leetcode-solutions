f7186f0bd0987cf7a9188ae566022264fa030971
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-02-03 18:30:38 +0530

    Time: 404 ms (28.66%), Space: 24.9 MB (21.09%) - LeetHub

---
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map <int,int> mp;
+        for(int i:nums3){
+            for(int j:nums4){
+                mp[i+j]++;
+            }
+        }
+        int count=0;
+        for(int k:nums1){
+            for(int l:nums2){
+                count += mp[-(k+l)];
+            }
+        }
+        return count;
+    }
+};
