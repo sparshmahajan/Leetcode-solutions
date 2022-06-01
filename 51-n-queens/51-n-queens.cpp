e746971442b529f351c8b81647c10bcb92224c0f
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-01 15:42:25 +0530

    Time: 14 ms (40.79%), Space: 11.2 MB (26.81%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,42 +1,48 @@
 class Solution {
 public:
-    bool isQueenSafe(vector <string> board,int row,int col,int n){
-        //for upper digonal
-        int duprow = row;
-        int dupcol = col;
-        while(row >=0 && col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row-- , col--;
-        }
+//     bool isQueenSafe(vector <string> board,int row,int col,int n){
+//         //for upper digonal
+//         int duprow = row;
+//         int dupcol = col;
+//         while(row >=0 && col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             row-- , col--;
+//         }
         
-        //for lower digonal
-        row = duprow;
-        col = dupcol;
-        while(row < n and col >= 0){
-            if(board[row][col] == 'Q') return false;
-            row++, col--;
-        }
+//         //for lower digonal
+//         row = duprow;
+//         col = dupcol;
+//         while(row < n and col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             row++, col--;
+//         }
         
-        //for left side
-        row = duprow;
-        col = dupcol;
-        while(col >= 0){
-            if(board[row][col] == 'Q') return false;
-            col--;
-        }
-        return true;
-    }
+//         //for left side
+//         row = duprow;
+//         col = dupcol;
+//         while(col >= 0){
+//             if(board[row][col] == 'Q') return false;
+//             col--;
+//         }
+//         return true;
+//     }
     
-    void solve(int col,int n,vector<string>& board,vector <vector<string>>& ans){
+    void solve(int col,int n,vector<string>& board,vector <vector<string>>& ans,vector <int> leftRow,vector <int> upperDiagonal,vector <int> lowerDiagonal){
         if(col == n){
             ans.push_back(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(isQueenSafe(board,row,col,n)){
+            if(leftRow[row] == 0 && lowerDiagonal[row+col] == 0 && upperDiagonal[n-1 + col-row] == 0){
                 board[row][col]='Q';
-                solve(col+1,n,board,ans);
+                leftRow[row]=1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1 + col-row]=1;
+                solve(col+1,n,board,ans,leftRow,upperDiagonal,lowerDiagonal);
                 board[row][col]='.';
+                leftRow[row]=0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1 + col-row]=0;
             }
         }
     }
@@ -47,7 +53,8 @@ public:
         string s(n,'.');
         for(int i=0;i<n;i++)
             board[i]=s;
-        solve(0,n,board,ans);
+        vector <int> leftRow(n,0),upperDiagonal(2*n-1,0),lowerDiagonal(2*n-1,0);
+        solve(0,n,board,ans,leftRow,upperDiagonal,lowerDiagonal);
         return ans;
     }
 };
