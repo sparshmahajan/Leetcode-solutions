740d2bb919e4c2c4d6b999d00bc39da6a953fe36
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-02 21:00:05 +0530

    Time: 17 ms (42.74%), Space: 10.6 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector <vector<int>> result(matrix[0].size(),vector <int> (matrix.size()));
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
