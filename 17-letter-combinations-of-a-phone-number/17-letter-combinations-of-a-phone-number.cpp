2b44ab00cb51f99083e8878a9d4a90a4e91fed39
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-19 18:59:20 +0530

    Time: 3 ms (41.39%), Space: 6.5 MB (60.53%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    void solve(int idx,string& digits,string s,vector<string>& ans,map <char,string>& mp){
+    void solve(int idx,string& digits,string s,vector<string>& ans,vector<string>& mp){
         if(idx == 0){
             ans.push_back(s);
             return;
         }
 
-        for(int i=0;i<mp[digits[idx-1]].size();i++){
-            string t = mp[digits[idx-1]];
+        int index = digits[idx-1] - '2';
+        for(int i=0;i<mp[index].size();i++){
+            string t = mp[index];
             solve(idx-1,digits,t[i]+s,ans,mp);
         }
     }
@@ -15,15 +16,7 @@ public:
         vector<string> ans;
         if(digits.size() == 0)
             return ans;
-        map <char,string> mp;
-        mp['2'] = "abc";
-        mp['3'] = "def";
-        mp['4'] = "ghi";
-        mp['5'] = "jkl";
-        mp['6'] = "mno";
-        mp['7'] = "pqrs";
-        mp['8'] = "tuv";
-        mp['9'] = "wxyz";
+        vector<string> mp = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         
         solve(digits.size(),digits,"",ans,mp);
         return ans;
