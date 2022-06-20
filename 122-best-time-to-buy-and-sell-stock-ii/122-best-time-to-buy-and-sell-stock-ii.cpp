5433f558c750f7287399d248fb642aa449958420
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-21 02:01:34 +0530

    Time: 28 ms (5.09%), Space: 15.3 MB (9.88%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -20,10 +20,8 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n+1,vector <int> (2,0));
         for(int idx=n-1;idx>=0;idx--){
-                dp[idx][1] = max(-prices[idx] + dp[idx+1][0], dp[idx+1][1]);
-                
-                dp[idx][0] = max(prices[idx] + dp[idx+1][1], dp[idx+1][0]);
-        
+            dp[idx][1] = max(-prices[idx] + dp[idx+1][0], dp[idx+1][1]);    
+            dp[idx][0] = max(prices[idx] + dp[idx+1][1], dp[idx+1][0]);
         }
         return dp[0][1];
     }
