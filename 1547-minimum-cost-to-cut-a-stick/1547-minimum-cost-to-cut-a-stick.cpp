299dcdb2d0f77446b0942ef275fae74820f32a58
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-21 15:37:18 +0530

    Time: 141 ms (28.16%), Space: 10.4 MB (17.94%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,29 @@
-class Solution {
-public:
-    int solve(int i,int j,vector <int>& cuts,vector<vector<int>>& dp){
-        if(i > j)
-            return 0;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int mini = 1e9;
-        for(int k=i; k<=j;k++){
-            int cost = (cuts[j+1] - cuts[i-1]) + solve(i,k-1,cuts,dp) + solve(k+1,j,cuts,dp);
-            mini = min(cost,mini);
+class Solution
+{
+    public:
+        int minCost(int n, vector<int> &cuts)
+        {
+            int c = cuts.size();
+            cuts.push_back(n);
+            cuts.insert(cuts.begin(), 0);
+            sort(cuts.begin(), cuts.end());
+            vector<vector < int>> dp(c + 2, vector<int> (c + 2, 0));
+
+            for (int i = c; i >= 1; i--)
+            {
+                for (int j = 1; j <= c; j++)
+                {
+                    if (i > j) continue;
+
+                    int mini = 1e9;
+                    for (int k = i; k <= j; k++)
+                    {
+                        int cost = (cuts[j + 1] - cuts[i - 1]) + dp[i][k - 1] + dp[k + 1][j];
+                        mini = min(cost, mini);
+                    }
+                    dp[i][j] = mini;
+                }
+            }
+            return dp[1][c];
         }
-        return dp[i][j] = mini;
-    }
-    
-    int minCost(int n, vector<int>& cuts) {
-        int c = cuts.size();
-        cuts.push_back(n);
-        cuts.insert(cuts.begin(),0);
-        sort(cuts.begin(),cuts.end());
-        vector<vector<int>> dp(c+1,vector <int> (c+1,-1));
-        return solve(1,c,cuts,dp);
-    }
 };
