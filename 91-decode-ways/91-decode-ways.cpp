244a9a786ae1481c97183636a42f8b9c45616cb9
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-23 16:18:38 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (44.54%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,29 +1,31 @@
-class Solution {
-public:
-    int solve(int idx ,string& s,vector <int>& dp){
-        if(idx >= s.size())
-            return 1;
-        
-        if(dp[idx] != -1)
-            return dp[idx];
-        
-        int digit = s[idx] - '0';
-        if(digit == 0)
-            return 0;
-        
-        int ans = solve(idx+1,s,dp);
-        
-        if(idx+1 < s.size()){
-            int digit2 = s[idx+1] - '0';
-            int val = digit * 10 + digit2;
-            if(val <= 26)
-                ans += solve(idx+2,s,dp);
+class Solution
+{
+    public:
+
+        int numDecodings(string s)
+        {
+            int n = s.size();
+            vector<int> dp(n+1, 0);
+            dp[n] = 1;
+            for (int idx = n - 1; idx >= 0; idx--)
+            {
+                int digit = s[idx] - '0';
+                if (digit == 0)
+                    dp[idx] = 0;
+                else
+                {
+                    int ans = dp[idx + 1];
+
+                    if (idx + 1 < s.size())
+                    {
+                        int digit2 = s[idx + 1] - '0';
+                        int val = digit *10 + digit2;
+                        if (val <= 26)
+                            ans += dp[idx + 2];
+                    }
+                    dp[idx] = ans;
+                }
+            }
+            return dp[0];
         }
-        return dp[idx] = ans;
-        
-    }
-    int numDecodings(string s) {
-        vector <int> dp(s.size(),-1);
-        return solve(0,s,dp);
-    }
 };
