d7fcf69799baec8b4b98c46b57b652b2413d3487
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-23 19:25:58 +0530

    Time: 415 ms (51.49%), Space: 11.9 MB (94.07%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -5,19 +5,20 @@ class Solution
     {
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector < int>> dp(n + 1, vector<int> (m + 1, 0));
+        vector<int> prev(m + 1, 0) , curr (m+1,0);
         int ans = 0;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= m; j++)
             {
                 if (nums1[i - 1] == nums2[j - 1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                    ans = max(ans,dp[i][j]);
+                    curr[j] = 1 + prev[j-1];
+                    ans = max(ans,curr[j]);
                 }
                 else
-                    dp[i][j] = 0;
+                    curr[j] = 0;
             }
+            prev = curr;
         }
         return ans;
     }
