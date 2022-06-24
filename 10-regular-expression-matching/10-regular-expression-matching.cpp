3d90af2763afef642d21a7ba82c901be351d860a
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-24 22:38:03 +0530

    Time: 3 ms (90.98%), Space: 6.5 MB (73.09%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -4,14 +4,14 @@ class Solution
         bool isMatch(string s, string p)
         {
             int n = s.size(), m = p.size();
-            vector<vector < bool>> dp(n + 1, vector<bool> (m + 1, false));
-            dp[0][0] = true;
+            vector<bool> prev(m + 1, false) , curr(m+1,false);
+            prev[0] = true;
             for (int j = 2; j <= m; j++)
             {
                 if (p[j - 1] == '*')
-                    dp[0][j] = dp[0][j - 2];
+                    prev[j] = prev[j - 2];
                 else
-                    dp[0][j] = false;
+                    prev[j] = false;
             }
 
             for (int i = 1; i <= n; i++)
@@ -19,23 +19,24 @@ class Solution
                 for (int j = 1; j <= m; j++)
                 {
                     if (s[i - 1] == p[j - 1] or p[j - 1] == '.')
-                        dp[i][j] = dp[i - 1][j - 1];
+                        curr[j] = prev[j - 1];
 
                     else if (p[j - 1] == '*')
                     {
                         if (p[j - 2] == s[i - 1] or p[j - 2] == '.')
                         {
-                            dp[i][j] = dp[i - 1][j] or dp[i][j - 2];
+                            curr[j] = prev[j] or curr[j - 2];
                         }
                         else
                         {
-                            dp[i][j] = dp[i][j - 2];
+                            curr[j] = curr[j - 2];
                         }
                     }
                     else
-                        dp[i][j] = false;
+                        curr[j] = false;
                 }
+                prev = curr;
             }
-            return dp[n][m];
+            return prev[m];
         }
 };
