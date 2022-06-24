94064b7967345b26e6071e2469b8cfbc5ddf7032
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-24 22:34:48 +0530

    Time: 3 ms (90.98%), Space: 7 MB (61.52%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,46 +1,41 @@
 class Solution
 {
     public:
-        bool solve(int i, int j, string &s, string &p, vector<vector < int>> &dp)
+        bool isMatch(string s, string p)
         {
-            if (i == 0 && j == 0)
-                return true;
-            if (i > 0 && j == 0)
-                return false;
-            
-            if (dp[i][j] != -1)
-                return dp[i][j];
-            
-            if (j > 0 && i == 0)
+            int n = s.size(), m = p.size();
+            vector<vector < bool>> dp(n + 1, vector<bool> (m + 1, false));
+            dp[0][0] = true;
+            for (int j = 2; j <= m; j++)
             {
-                if(p[j-1] == '*')
-                    return solve(i,j-2,s,p,dp);
-                return false;
-            }
-
-            if (s[i - 1] == p[j - 1] or p[j - 1] == '.')
-                return dp[i][j] = solve(i - 1, j - 1, s, p, dp);
-
-            if (p[j - 1] == '*')
-            {
-                if (p[j - 2] == s[i - 1] or p[j - 2] == '.')
-                {
-                    dp[i][j] = solve(i - 1, j, s, p, dp) or solve(i, j - 2, s, p, dp);
-                }
+                if (p[j - 1] == '*')
+                    dp[0][j] = dp[0][j - 2];
                 else
-                {
-                    dp[i][j] = solve(i, j - 2, s, p, dp);
-                }
-                return dp[i][j];
+                    dp[0][j] = false;
             }
 
-            return dp[i][j] = false;
-        }
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    if (s[i - 1] == p[j - 1] or p[j - 1] == '.')
+                        dp[i][j] = dp[i - 1][j - 1];
 
-    bool isMatch(string s, string p)
-    {
-        int n = s.size(), m = p.size();
-        vector<vector < int>> dp(n + 1, vector<int> (m + 1, -1));
-        return solve(n, m, s, p, dp);
-    }
+                    else if (p[j - 1] == '*')
+                    {
+                        if (p[j - 2] == s[i - 1] or p[j - 2] == '.')
+                        {
+                            dp[i][j] = dp[i - 1][j] or dp[i][j - 2];
+                        }
+                        else
+                        {
+                            dp[i][j] = dp[i][j - 2];
+                        }
+                    }
+                    else
+                        dp[i][j] = false;
+                }
+            }
+            return dp[n][m];
+        }
 };
