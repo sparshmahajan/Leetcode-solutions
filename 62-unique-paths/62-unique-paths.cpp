3828abf54c7fc8b4ec137dd453286e67ee7795d1
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-25 19:59:48 +0530

    Time: 6 ms (11.55%), Space: 6.6 MB (25.38%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,23 +1,28 @@
-class Solution {
-public:
-    int solve(int i,int j,vector<vector<int>>& dp){
-        if(i == 0 && j == 0)
-            return 1;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int move_up = 0;
-        int move_left = 0;
-        if(i>=1)
-            move_left = solve(i-1,j,dp);
-        if(j>=1)
-            move_up = solve(i,j-1,dp);
-        
-        return dp[i][j] = move_up + move_left;
-    }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        return solve(m-1,n-1,dp);
-    }
+class Solution
+{
+    public:
+        int uniquePaths(int m, int n)
+        {
+            vector<vector < int>> dp(m, vector<int> (n, 0));
+            for(int i=0;i<n;i++)
+                dp[0][i] = 1;
+            for(int i=0;i<m;i++)
+                dp[i][0] = 1;
+            for (int i = 1; i < m; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    int move_up = 0;
+                    int move_left = 0;
+                    if (i >= 1)
+                        move_left = dp[i - 1][j];
+                    if (j >= 1)
+                        move_up = dp[i][j - 1];
+
+                    if (i != 0 && j != 0)
+                        dp[i][j] = move_up + move_left;
+                }
+            }
+            return dp[m - 1][n - 1];
+        }
 };
