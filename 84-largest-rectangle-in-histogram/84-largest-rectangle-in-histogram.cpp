3cc52c6e4966bf3a8d271c7e155f68e27c2e16bc
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-26 20:24:23 +0530

    Time: 159 ms (98.62%), Space: 75.4 MB (91.48%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size(),i;
-        vector <int> left(n),right(n);
         stack <int> stk;
-        for(i=0;i<n;i++){
-            if(stk.empty()){
-                left[i]=0;
-                stk.push(i);
+        int maxArea = 0;
+        int n = heights.size();
+        for(int i=0;i<=n;i++){
+            while(!stk.empty() and (i==n or heights[stk.top()] >= heights[i])){
+                int height = heights[stk.top()];
+                stk.pop();
+                int width;
+                if(stk.empty())
+                    width = i;
+                else
+                    width = i- stk.top()- 1;
+                maxArea = max(maxArea,width * height);
             }
-            else{
-                while(!stk.empty() and heights[stk.top()] >= heights[i])
-                    stk.pop();
-                left[i] = stk.empty() ? 0 : stk.top()+1; 
-                stk.push(i);
-            }
+            stk.push(i);
         }
-        while(!stk.empty())
-            stk.pop();
-         for(i=n-1;i>=0;i--){
-            if(stk.empty()){
-                right[i]=n-1;
-                stk.push(i);
-            }
-            else{
-                while(!stk.empty() and heights[stk.top()] >= heights[i])
-                    stk.pop();
-                right[i] = stk.empty() ? n-1 : stk.top()-1; 
-                stk.push(i);
-            }
-        }
-        int max_area = 0;
-        for(i=0;i<n;i++){
-            int area = heights[i] * (right[i] - left[i] + 1);
-            max_area = max(max_area,area);
-        }
-        return max_area;
+        return maxArea;
     }
 };
