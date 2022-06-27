ade446d8c989cd52bc6012f07e85ce62478b6464
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-27 17:37:05 +0530

    Time: 7 ms (94.60%), Space: 6.4 MB (94.78%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,29 +1,33 @@
-class Solution {
-public:
-    int solve(int i,int j,string& word1, string& word2,vector<vector<int>>& dp){
-        if(i == 0)
-            return j;
-        if(j == 0)
-            return i;
-        
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        int ans = INT_MAX;
-        if(word1[i-1] == word2[j-1])
-            return solve(i-1,j-1,word1,word2,dp);
-        
-        int insert = 1 + solve(i,j-1,word1,word2,dp);
-        int del = 1 + solve(i-1,j,word1,word2,dp);
-        int replace = 1 + solve(i-1,j-1,word1,word2,dp);
-        
-        ans = min(insert,min(del,replace));
-        return dp[i][j] = ans;
-    }
-    
-    int minDistance(string word1, string word2) {
-        int n = word1.size() , m = word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,word1,word2,dp);
-    }
+class Solution
+{
+    public:
+        int minDistance(string word1, string word2)
+        {
+            int n = word1.size(), m = word2.size();
+            vector<int> prev(m + 1, 0),curr(m+1,0);
+            for (int j = 0; j <=m; j++)
+                prev[j] = j;
+
+            for (int i = 1; i <= n; i++)
+            {
+                curr[0] = i;
+                for (int j = 1; j <= m; j++)
+                {
+                    int ans = INT_MAX;
+                    if (word1[i - 1] == word2[j - 1])
+                        ans = prev[j - 1];
+                    else
+                    {
+                        int insert = 1 + curr[j - 1];
+                        int del = 1 + prev[j];
+                        int replace = 1 + prev[j - 1];
+
+                        ans = min(insert, min(del, replace));
+                    }
+                    curr[j] = ans;
+                }
+                prev = curr;
+            }
+            return prev[m];
+        }
 };
