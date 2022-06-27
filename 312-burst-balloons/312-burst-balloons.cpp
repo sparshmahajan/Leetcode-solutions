8935a80497c0c06af65e9882f8373acf208733af
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-27 17:02:39 +0530

    Time: 565 ms (59.56%), Space: 10.3 MB (51.82%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,26 +1,30 @@
 class Solution
 {
     public:
-        int solve(int i, int j,vector<int> &nums,vector<vector<int>>& dp){
-            if(i>j)
-                return 0;
-            
-            if(dp[i][j] != -1)
-                return dp[i][j];
-            
-            int maxCoins = INT_MIN;
-            for(int k=i;k<=j;k++){
-                int cost = nums[i-1] * nums[k] * nums[j+1] + solve(i,k-1,nums,dp) + solve(k+1,j,nums,dp);
-                maxCoins = max(maxCoins,cost);
-            }
-            return dp[i][j] = maxCoins;
-        }
-
-    int maxCoins(vector<int> &nums) {
+    int maxCoins(vector<int> &nums)
+    {
         int n = nums.size();
-        nums.insert(nums.begin(),1);
+        nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(1,n,nums,dp);
+        vector<vector < int>> dp(n + 2, vector<int> (n + 2, 0));
+        for (int i = n; i >= 1; i--)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (i > j)
+                    continue;
+                else
+                {
+                    int maxCoins = INT_MIN;
+                    for (int k = i; k <= j; k++)
+                    {
+                        int cost = nums[i - 1] *nums[k] *nums[j + 1] + dp[i][k - 1] + dp[k + 1][j];
+                        maxCoins = max(maxCoins, cost);
+                    }
+                    dp[i][j] = maxCoins;
+                }
+            }
+        }
+        return dp[1][n];
     }
 };
