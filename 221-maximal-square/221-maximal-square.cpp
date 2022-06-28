39da8657978c08b9543a1311e8caf65f201f67b8
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-28 23:08:45 +0530

    Time: 79 ms (73.92%), Space: 16.5 MB (92.21%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -2,25 +2,29 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size() , m = matrix[0].size();
-        vector<vector<int>> dp(n,vector<int> (m,0));
-        for(int i=0;i<n;i++)
-            dp[i][0] = matrix[i][0] - '0';
-        for(int i=0;i<m;i++)
-            dp[0][i] = matrix[0][i]- '0';
-        
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(matrix[i][j] == '1'){
-                    dp[i][j] = min(dp[i][j-1],min(dp[i-1][j],dp[i-1][j-1])) + 1;
-                }
-            }
-        }
+        vector<int> prev(m,0),curr(m,0);
         
         int area = 0;
-        for(auto x:dp){
-            for(auto y:x)
-                area = max(area,y);
+        for(int i=0;i<m;i++){
+            prev[i] = matrix[0][i]- '0';
+            area = max(prev[i],area);
         }
+
+        for(int i=1;i<n;i++){
+            curr[0] = matrix[i][0] - '0';
+            area = max(curr[0],area);
+            for(int j=1;j<m;j++){
+                if(matrix[i][j] == '1'){
+                    curr[j] = min(curr[j-1],min(prev[j],prev[j-1])) + 1;
+                    area = max(area,curr[j]);
+                }
+                else{
+                    curr[j] = 0;
+                }
+            }
+            prev = curr;
+        }
+        
         return area * area;
     }
 };
