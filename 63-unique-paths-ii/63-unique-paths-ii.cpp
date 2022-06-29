5f1f694caec56f1565894764a8a48f459536caaf
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-30 00:35:05 +0530

    Time: 3 ms (83.37%), Space: 7.6 MB (75.56%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -4,43 +4,36 @@ class Solution
         int uniquePathsWithObstacles(vector<vector < int>> &obstacleGrid)
         {
             int m = obstacleGrid.size(), n = obstacleGrid[0].size();
-            vector<vector < int>> dp(m, vector<int> (n, 0));
-            dp[0][0] = obstacleGrid[0][0] ? 0 : 1;
+            vector<int> prev(n, 0),curr(n,0);
+            prev[0] = obstacleGrid[0][0] ? 0 : 1;
             for (int i = 1; i < n; i++)
             {
                 if (obstacleGrid[0][i] != 1)
-                    dp[0][i] = dp[0][i - 1];
-                else
-                    dp[0][i] = 0;
-            }
-            for (int i = 1; i < m; i++)
-            {
-                if (obstacleGrid[i][0] != 1)
-                    dp[i][0] = dp[i - 1][0];
-                else
-                    dp[i][0] = 0;
+                    prev[i] = prev[i - 1];
             }
 
             for (int i = 1; i < m; i++)
             {
+                curr[0] = obstacleGrid[i][0] ? 0 : prev[0];
                 for (int j = 1; j < n; j++)
                 {
                     if (obstacleGrid[i][j] == 1)
-                        dp[i][j] = 0;
+                        curr[j] = 0;
                     else
                     {
                         int move_up = 0;
                         int move_left = 0;
                         if (i >= 1)
-                            move_left = dp[i - 1][j];
+                            move_left = prev[j];
                         if (j >= 1)
-                            move_up = dp[i][j - 1];
+                            move_up = curr[j - 1];
 
-                        dp[i][j] = move_up + move_left;
+                        curr[j] = move_up + move_left;
                     }
                 }
+                prev = curr;
             }
 
-            return dp[m - 1][n - 1];
+            return prev[n - 1];
         }
 };
