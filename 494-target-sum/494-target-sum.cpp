18bcc927ae4e832c337293465600158d47e7ef60
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-06-30 02:38:25 +0530

    Time: 2375 ms (5.03%), Space: 228.7 MB (5.03%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int solve(int i,int expression,vector<int>& nums, int target,vector<map<int,int>>& dp){
-        if(i == nums.size())
-            return target == expression;
-        
-        if(dp[i].find(expression) != dp[i].end())
-            return dp[i][expression];
-        
-        int add = solve(i+1,expression+nums[i],nums,target,dp);
-        int minus = solve(i+1,expression-nums[i],nums,target,dp);
-        
-        return dp[i][expression] =add+minus;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        vector<map<int,int>> dp(nums.size());
-        return solve(0,0,nums,target,dp);
+        int max_sum = accumulate(nums.begin(),nums.end(),0);
+        int n = nums.size();
+        vector<map<int,int>> dp(n+1);
+        dp[n][target] = 1;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int expression=max_sum;expression >= -max_sum;expression--){
+                int add = dp[i+1][expression+nums[i]];
+                int minus = dp[i+1][expression-nums[i]];
+                
+                dp[i][expression] =add+minus;
+            }
+        }
+        return dp[0][0];
     }
 };
