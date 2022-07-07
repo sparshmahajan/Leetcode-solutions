f917f6d575d10d3e028eed746a8405ff3c330ff9
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-07-08 04:28:56 +0530

    Time: 33 ms (85.96%), Space: 6.5 MB (82.20%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -4,23 +4,24 @@ class Solution
         int numDistinct(string s, string t)
         {
             int m = s.size(), n = t.size();
-            vector<vector <unsigned long>> dp(m + 1, vector<unsigned long> (n + 1, 0));
-            for (int i = 0; i <= m; i++)
-                dp[i][0] = 1;
+            vector<unsigned long> prev(n + 1, 0),curr(n+1,0);
+            prev[0] = 1;
 
             for (int i = 1; i <= m; i++)
             {
+                curr[0] = 1;
                 for (int j = 1; j <= n; j++)
                 {
                     unsigned long pick = 0, not_pick = 0;
                     
                     if (s[i - 1] == t[j - 1])
-                        pick = dp[i - 1][j - 1];
-                    not_pick = dp[i - 1][j];
+                        pick = prev[j - 1];
+                    not_pick = prev[j];
 
-                    dp[i][j] = pick + not_pick; 
+                    curr[j] = pick + not_pick; 
                 }
+                prev = curr;
             }
-            return dp[m][n];
+            return prev[n];
         }
 };
