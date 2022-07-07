d188a973a1b301facaaeaa0e9dac8c9141f51dfa
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-07-07 16:50:57 +0530

    Time: 6 ms (61.58%), Space: 7 MB (56.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,31 +1,31 @@
 class Solution
 {
     public:
-        bool solve(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector < int>> &dp)
+        bool isInterleave(string s1, string s2, string s3)
         {
-            if (k == 0 and i == 0 and j == 0)
+            if (s1.size() + s2.size() != s3.size())
+                return false;
+            vector<vector < int>> dp(s1.size() + 1, vector<int> (s2.size() + 1, 0));
+            dp[0][0] = 1;
+            for (int i = 0; i <= s1.size(); i++)
             {
-                return true;
+                for (int j = 0; j <= s2.size(); j++)
+                {
+                    if (i == 0 and j == 0)
+                    {
+                        dp[i][j] = 1;
+                        continue;
+                    }
+                    if (i > 0 and s1[i - 1] == s3[i + j - 1] and j > 0 and s2[j - 1] == s3[i + j - 1])
+                        dp[i][j] = dp[i - 1][j] or dp[i][j - 1];
+                    else if (i > 0 && s1[i - 1] == s3[i + j - 1])
+                        dp[i][j] = dp[i - 1][j];
+                    else if (j > 0 && s2[j - 1] == s3[i + j - 1])
+                        dp[i][j] = dp[i][j - 1];
+                    else
+                        dp[i][j] = false;
+                }
             }
-
-            if (i > 0 and j > 0 and dp[i][j] != -1)
-                return dp[i][j];
-
-            if (i > 0 and s1[i - 1] == s3[k - 1] and j > 0 and s2[j - 1] == s3[k - 1])
-                return dp[i][j] = solve(i - 1, j, k - 1, s1, s2, s3, dp) or solve(i, j - 1, k - 1, s1, s2, s3, dp);
-            else if (i > 0 && s1[i - 1] == s3[k - 1])
-                return dp[i][j] = solve(i - 1, j, k - 1, s1, s2, s3, dp);
-            else if (j > 0 && s2[j - 1] == s3[k - 1])
-                return dp[i][j] = solve(i, j - 1, k - 1, s1, s2, s3, dp);
-            else
-                return dp[i][j] = false;
+            return dp[s1.size()][s2.size()];
         }
-
-    bool isInterleave(string s1, string s2, string s3)
-    {
-        if (s1.size() + s2.size() != s3.size())
-            return false;
-        vector<vector < int>> dp(s1.size() + 1, vector<int> (s2.size() + 1, -1));
-        return solve(s1.size(), s2.size(), s3.size(), s1, s2, s3, dp);
-    }
 };
