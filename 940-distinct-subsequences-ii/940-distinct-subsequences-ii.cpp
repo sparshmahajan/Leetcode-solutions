7774cd73c5c6c06063de0b4588396d9f8cbd94df
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-07-09 00:44:30 +0530

    Time: 475 ms (5.06%), Space: 18.1 MB (6.18%) - LeetHub

--- 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -2,29 +2,22 @@ class Solution
 {
     public:
         int mod = 1e9 + 7;
-    int solve(string &s, int i,vector<unsigned long> &dp)
-    {
-        if (i == s.size())
-            return 0;
-        
-        if(dp[i] != -1)
-            return dp[i];
-
-        vector<int> freq(26, 0);
-        unsigned long ans = 0;
-        for (int j = i; j < s.size(); j++)
-        {
-            if (freq[s[j] - 'a'])
-                continue;
-            freq[s[j] - 'a'] = 1;
-            ans = ans + 1 + solve(s, j + 1,dp) % mod;
-        }
-        return dp[i] = ans % mod;
-    }
-
     int distinctSubseqII(string s)
     {
-        vector<unsigned long> dp(s.size(),-1);
-        return solve(s, 0,dp);
+        vector < unsigned long > dp(s.size() + 1, 0);
+        for (int i = s.size() - 1; i >= 0; i--)
+        {
+            vector<int> freq(26, 0);
+            unsigned long ans = 0;
+            for (int j = i; j < s.size(); j++)
+            {
+                if (freq[s[j] - 'a'])
+                    continue;
+                freq[s[j] - 'a'] = 1;
+                ans = ans + 1 + dp[j + 1] % mod;
+            }
+            dp[i] = ans % mod;
+        }
+        return dp[0];
     }
 };
