5e610ec0d2fe55c2d3bedd1c90d4155799f399df
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-10-23 19:42:16 +0530

    Time: 155 ms (18.97%), Space: 30.7 MB (35.64%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        int totalSum = accumulate(nums.begin(),nums.end(),0);
+        unordered_set <int> st;
+        for(int i : nums)
+            st.insert(i);
+        int sum = accumulate(st.begin(),st.end(),0);
+        
+        int repeatedElement = totalSum - sum;
         int n = nums.size();
-        vector <int> res;
-        map <int,int> mp;
-        for(int i=0;i<n;i++)
-            mp[nums[i]]++;
-        for(int i=0;i<n;i++){
-            if(mp[nums[i]] == 2){
-                res.push_back(nums[i]);
-                break;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(mp[i+1] == 0){
-                res.push_back(i+1);
-                break;
-            }
-        }
-        return res;
+        int missingElement = (n * (n+1))/2 - sum;
+        return {repeatedElement , missingElement};
     }
 };
