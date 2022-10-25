2d8ece8625bf30fadbc219c9fa1228e352988ac0
Sparsh Mahajan <sparshmahajan3@gmail.com>
2022-10-25 16:19:46 +0530

    Time: 490 ms (89.24%), Space: 160.8 MB (10.22%) - LeetHub

--- 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
+++ 2438-range-product-queries-of-powers/2438-range-product-queries-of-powers.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> productQueries(int n, vector<vector<int>>& queries) {
         int  m = 1e9+7;
-        vector<int> bint, ans;
+        vector<int> powers, ans;
         //create power array 
         for(int i = 0; i < 32; ++i){
-            if((n & (1<<i)) != 0) bint.push_back(1<<i);
+            if((n & (1<<i)) != 0) 
+                powers.push_back(1<<i);
         }
         for(auto q: queries){
             int i = q[0];
-            long p = bint[i++];
+            long p = powers[i++];
             while(i <= q[1]){
-                p = (p * bint[i++])%m;
+                p = (p * powers[i++])%m;
             }
             ans.push_back(p);
         }
